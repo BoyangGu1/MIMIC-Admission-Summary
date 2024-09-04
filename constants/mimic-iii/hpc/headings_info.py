eee566b25b73154674a2b0d01172ed76bd5e9670
Boyang Gu <boyang.gu@foxmail.com>
2024-09-04 10:24:38 +0100

    Create headings_info.py

---
+++ constants/mimic-iii/hpc/headings_info.py
@@ -0,0 +1,274 @@
+headings: list[str] = [
+    "Brief Hospital Course",
+    "HOSPITAL COURSE",
+    "SUMMARY OF HOSPITAL COURSE BY SYSTEMS",
+    "HOSPITAL COURSE BY SYSTEMS",
+    "SUMMARY OF HOSPITAL COURSE",
+    "HOSPITAL COURSE BY SYSTEM",
+    "HOSPITAL COURSE BY SYSTEMS INCLUDING PERTINENT LABORATORYDATA",
+    "BRIEF SUMMARY OF HOSPITAL COURSE",
+    "CONCISE SUMMARY OF HOSPITAL COURSE BY ISSUE/SYSTEM",
+    "HOSPITAL COURSE BY ISSUE/SYSTEM",
+    "SUMMARY OF HOSPITAL COURSE BY SYSTEM",
+    "HISTORY OF HOSPITAL COURSE BY SYSTEMS",
+    "CONCISE SUMMARY OF HOSPITAL COURSE",
+    "BRIEF HOSPITAL COURSE",
+    "REVIEW OF HOSPITAL COURSE BY SYSTEMS",
+    "HOSPITAL COURSE BY PROBLEM",
+    "BRIEF SUMMARY OF HOSPITAL COURSE BY ISSUE/SYSTEM",
+    "SUMMARY OF HOSPITAL COURSE BY ISSUE/SYSTEM",
+    "Hospital Course",
+    "HOSPITAL COURSE BY PROBLEMS",
+    "Hospital course is reviewed below by problem",
+    "Hospital course",
+    "HISTORY OF HOSPITAL COURSE",
+    "HISTORY OF HOSPITAL COURSE BY SYSTEM",
+    "HOSPITAL COURSE BY SYSTEM INCLUDING PERTINENT LABORATORYDATA",
+    "REVIEW OF HOSPITAL COURSE BY SYSTEM"
+]
+
+
+next_headings: list[str] = [
+    "Medications on Admission",
+    "CONDITION ON DISCHARGE",
+    "None",
+    "DISCHARGE MEDICATIONS",
+    "CONDITION AT DISCHARGE",
+    "DISCHARGE DIAGNOSES",
+    "Discharge Medications",
+    "DISCHARGE CONDITION",
+    "DISCHARGE STATUS",
+    "MEDICATIONS ON DISCHARGE",
+    "DISPOSITION",
+    "DISCHARGE DIAGNOSIS",
+    "DISCHARGE DISPOSITION",
+    "Discharge Disposition",
+    "DISCHARGE INSTRUCTIONS",
+    "CONDITION ON TRANSFER",
+    "PHYSICAL EXAMINATION ON DISCHARGE",
+    "NOTE",
+    "CONDITION AT TRANSFER",
+    "FINAL DIAGNOSES",
+    "DISCHARGE PHYSICAL EXAMINATION",
+    "FINAL DIAGNOSIS",
+    "FOLLOW-UP",
+    "PHYSICAL EXAMINATION",
+    "LABORATORY DATA",
+    "PRIMARY PEDIATRICIAN",
+    "TRANSITIONS OF CARE",
+    "MEDICATIONS",
+    "PAST MEDICAL HISTORY",
+    "FOLLOW UP",
+    "CONDITION",
+    "DIAGNOSES",
+    "RECOMMENDATIONS AFTER DISCHARGE",
+    "DISPO",
+    "CONDITION AT TIME OF TRANSFER",
+    "CAUSE OF DEATH",
+    "DISCHARGE PLAN",
+    "CARE AND RECOMMENDATIONS",
+    "CONDITION OF DISCHARGE",
+    "PENDING RESULTS",
+    "NAME OF PRIMARY PEDIATRICIAN",
+    "PENDING LABS",
+    "CARE RECOMMENDATIONS",
+    "FINAL DISCHARGE DIAGNOSES",
+    "TRANSITIONAL CARE",
+    "EMERGENCY CONTACT",
+    "ACTIVE DIAGNOSES",
+    "RECOMMENDATIONS",
+    "DISCHARGE FOLLOWUP",
+    "MEDICATIONS AT THE TIME OF DISCHARGE",
+    "DISCHARGE INSTRUCTIONS/FOLLOWUP",
+    "FOLLOW-UP INSTRUCTIONS",
+    "TRANSITION OF CARE",
+    "DIAGNOSIS",
+    "LABORATORY DATA ON DISCHARGE",
+    "COMM",
+    "ACUTE DIAGNOSES",
+    "CONDITION AT TIME OF DICTATION",
+    "PHYSICAL EXAMINATION AT DISCHARGE",
+    "LABS ON DISCHARGE",
+    "INTERIM DIAGNOSES",
+    "MEDICATIONS ON TRANSFER",
+    "PRIMARY PEDIATRIC CARE",
+    "DISCHARGE DISPOSITION/CONDITION",
+    "LABORATORY",
+    "CONTACT",
+    "MEDICATIONS AT DISCHARGE",
+    "CURRENT MEDICATIONS",
+    "DISCHARGE LABORATORY DATA",
+    "LABORATORIES ON DISCHARGE",
+    "MEDICATIONS ON ADMISSION",
+    "Discharge disposition",
+    "Medications on discharge",
+    "Medications on admission"
+]
+
+
+# Issues and Problems
+issues_problems: list[str] = [
+    "ISSUES",
+    "ACTIVE ISSUES",
+    "INACTIVE ISSUES",
+    "ACTIVE ISSUES BY PROBLEM",
+    "INACTIVE ISSUES BY PROBLEM",
+    "ACUTE ISSUES",
+    "ACTIVE PROBLEMS",
+    "CHRONIC ISSUES",
+    "TRANSITIONAL ISSUES",
+    "TRANSITIONAL CARE ISSUES",
+    "TRANSITION OF CARE ISSUES",
+]
+
+# Systems and Specialties
+systems_specialties: list[str] = [
+    "GI",
+    "GU",
+    "CV",
+    "ID",
+    "NEURO",
+    "MSK",
+    "CVS",
+    "RESP",
+    "HEHE",
+    "PULM",
+    "ENDO",
+    "ENT",
+    "NEUROLOGY",
+    "HEME",
+    "CARDIOVASCULAR",
+    "PULMONARY",
+    "GASTROINTESTINAL",
+    "ENDOCRINE",
+    "RESPIRATORY",
+    "GENITOURINARY",
+    "HEMATOLOGY",
+    "INFECTIOUS DISEASE",
+    "CARDIAC",
+    "RENAL",
+    "NEUROLOGICAL",
+    "HEMATOLOGICAL",
+    "AUDIOLOGY",
+    "HEMATOLOGIC",
+    "OPHTHALMOLOGY",
+    "GENETICS",
+    "MICRO",
+    "CARDIOLOGY",
+    "SENSORY - AUDIOLOGY",
+    "SENSORY",
+    "NEUROLOGIC",
+    "PSYCHOSOCIAL",
+    "IMMUNIZATIONS",
+]
+
+# Conditions and Diseases
+conditions_diseases: list[str] = [
+    "HTN",
+    "CAD",
+    "UTI",
+    "CHF",
+    "COPD",
+    "DM",
+    "ARF",
+    "NSTEMI",
+    "DKA",
+    "GERD",
+    "DVT",
+    "SEIZURE D/O",
+    "ANEMIA (ACUTE BLOOD LOSS and RENAL)",
+    "HYPOTHYROID",
+    "HYPERGLYCEMIA",
+    "HIV",
+    "BPH",
+    "DMII",
+    "OSA",
+    "DIABETES",
+    "TLD",
+    "SOB",
+    "ABD",
+    "SVT",
+    "PROPHYLAXIS",
+    "RHYTHM",
+    "MYOCARDIUM",
+    "HEENT",
+    "ID/WOUND",
+    "ESRD",
+    "GIB",
+    "CORONARY ARTERY DISEASE",
+    "HYPERTENSION",
+]
+
+# Procedures and Treatments
+procedures_treatments: list[str] = [
+    "PUMP",
+    "PPX",
+    "PT",
+    "CXR",
+    "IV",
+    "IV ACCESS",
+    "ACCESS",
+    "ECHO",
+    "PROCEDURES",
+    "PROCEDURE",
+]
+
+# Courses
+courses: list[str] = [
+    "MICU COURSE",
+    "FLOOR COURSE",
+    "ICU COURSE",
+    "CCU COURSE",
+    "SURGICAL COURSE",
+]
+
+# Miscellaneous
+miscellaneous: list[str] = [
+    "TLD",
+    "SOB",
+    "ABD",
+    "SVT",
+    "PROPHYLAXIS",
+    "RHYTHM",
+    "MYOCARDIUM",
+    "HEENT",
+    "ID/WOUND",
+    "A/P",
+    "N",
+    "B",
+
+    "FEN",
+    "F/E/N",
+    "FLUIDS, ELECTROLYTES AND NUTRITION",
+    "FLUIDS, ELECTROLYTES, AND NUTRITION",
+    "FLUIDS AND ELECTROLYTES",
+    "FLUID AND ELECTROLYTES",
+    "FLUIDS, ELECTROLYTES, NUTRITION",
+    "FLUID, ELECTROLYTES AND NUTRITION",
+    "NUTRITION",
+]
+
+# Other Terms
+other_terms: list[str] = [
+    "IMPRESSION",
+    "PROBLEM LIST",
+    "PLAN",
+    "CODE",
+    "CODE STATUS",
+]
+
+ignore_headings: list[str] = (
+    issues_problems +
+    systems_specialties +
+    conditions_diseases +
+    procedures_treatments +
+    courses +
+    miscellaneous +
+    other_terms
+)
+
+heading_keyword: str = "hospital course"
+headings_dict: dict[str, int] = {}
+
+for i, heading in enumerate(headings):
+    headings_dict[heading] = i
